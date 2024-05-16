8edacdba098f3c8ca43bd8fbd61cc17139c9c6a2
AncientMeme <hatredwraith@gmail.com>
2024-05-16 20:00:59 +0800

    Added hover effect to ProjectCard

--- src/components/Project/ProjectCard.js
+++ src/components/Project/ProjectCard.js
@@ -25,32 +25,35 @@ function ProjectCard({image, imageAlt, title, githubHref=null, Content}) {
 
   return(
     <motion.div 
-      className="p-6 flex flex-col border-2 rounded border-dark-accent md:flex-row"
       initial={offscreen}
       whileInView={onscreen}
       viewport={{ once: true, amount: 0.2 }}
     >
-      <div className="flex items-center md:w-1/2">
-        <img
-          className="mb-2 md:mb-0 md:p-8 md:w-full"
-          src={image}
-          alt={imageAlt}
-        />
-      </div>
-      <div className="flex flex-col md:px-8 md:w-1/2">
-        <div className="md:grow">
-          <h2 className="mb-2 text-3xl text-dark-accent text-center md:text-left">{title}</h2>
-          {Content}
-        </div>
-        <nav className="mt-4 flex justify-around">
-          <LinkButton
-            id="1"
-            Icon={<AiFillGithub className="text-2xl"/>}
-            text={"Source"}
-            href={githubHref}
+      <section className="p-6 flex flex-col md:flex-row border-2 rounded border-dark-accent 
+        transition-transform ease-out delay-300 hover:scale-105"
+      >
+        <div className="flex items-center md:w-1/2">
+          <img
+            className="mb-2 md:mb-0 md:p-8 md:w-full"
+            src={image}
+            alt={imageAlt}
           />
-        </nav>
-      </div>
+        </div>
+        <div className="flex flex-col md:px-8 md:w-1/2">
+          <div className="md:grow">
+            <h2 className="mb-2 text-3xl text-dark-accent text-center md:text-left">{title}</h2>
+            {Content}
+          </div>
+          <nav className="mt-4 flex justify-around">
+            <LinkButton
+              id="1"
+              Icon={<AiFillGithub className="text-2xl"/>}
+              text={"Source"}
+              href={githubHref}
+            />
+          </nav>
+        </div>
+      </section>
     </motion.div>
   );
 }
