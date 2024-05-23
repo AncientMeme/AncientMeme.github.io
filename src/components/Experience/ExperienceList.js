2c50f61d7fa1367c1470df5eba0f5f9774ffc4b0
AncientMeme <hatredwraith@gmail.com>
2024-05-23 10:25:30 +0800

    Moved Highlight.js to component

--- src/components/Experience/ExperienceList.js
+++ src/components/Experience/ExperienceList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Highlight from "../../Highlight.js";
+import Highlight from "../Highlight.js";
 import TextLink from "../TextLink.js";
 const ExperienceList = [
   {
---
+++ src/components/Highlight.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Highlight({text}) {
+  return <span className="text-dark-accent">{text}</span>
+}
+
+export default Highlight;
